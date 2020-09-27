41be217bb705305eb3627544db7e51017a12367c
Emad Darabeh <emad.dar97@gmail.com>
2020-09-27 11:08:25 +0300

    created client deployment

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // on the server
-
+    const SERVICE_NAME = 'ingress-nginx-controller';
+    const NAMESPACE = 'ingress-nginx';
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: `http://${SERVICE_NAME}.${NAMESPACE}.svc.cluster.local`,
       headers: req.headers,
     });
   } else {
